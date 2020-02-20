7c55d7d062782f127594fa3828c7571589221cf6
Justinayeung <yeunj511@newschool.edu>
2020-02-20 14:00:25 -0500

    Unity_Touch_LiveCoding

---
+++ Unity/MobileTesting/Assets/FXController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FXController : MonoBehaviour
+{
+    public ParticleSystem exPrefab;
+
+    /// <summary>
+    /// Spawns a particle system
+    /// </summary>
+    /// <param name="pos"></param>
+    /// <returns></returns>
+    public ParticleSystem SpawnExplosion(Vector3 pos) {
+        //Instantiate returns a Gameobject so use as ParticleSystem so that there are no errors (return a ParticleSystem)
+        ParticleSystem effect = Instantiate(exPrefab, pos, Quaternion.identity, this.transform) as ParticleSystem;
+        Destroy(effect.gameObject, 10f); //You can do effect.main.duration
+        return effect;
+    }
+}
---
+++ Unity/MobileTesting/Assets/FXManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FXManager : MonoBehaviour
+{
+    public FXController fxMan;
+
+    // Start is called before the first frame update
+    void Start()  {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        for (int i = 0; i < Input.touchCount; i++) {
+            //Input.touches[i] //Gives you current touch, accessing it through an array
+            Touch touch = Input.GetTouch(i);
+
+            //If screen is tapped than spawn the explosion
+            if (touch.phase == TouchPhase.Ended && touch.tapCount == 1) {
+                fxMan.SpawnExplosion(Camera.main.ScreenToWorldPoint(touch.position)); //This is like a mouse event, translates the point to the world space
+            }
+        }
+    }
+}
