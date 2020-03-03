d9f9390a1a3c1ff2199d357f42503caf116004eb
Justinayeung <yeunj511@newschool.edu>
2020-03-03 15:47:26 -0500

    Projected 1

---
+++ Unity/ProjectedEnvironments/Assets/Scripts/CameraEffect.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class CameraEffect : MonoBehaviour
+{
+    public PostProcessVolume pp;
+    ChromaticAberration chrome;
+    Vignette vig;
+    LensDistortion dis;
+    AmbientOcclusion amo;
+
+    public Material[] wallMaterial;
+    public Renderer[] walls;
+    bool change = false;
+
+    // Start is called before the first frame update
+    void Start() {
+        pp.profile.TryGetSettings(out chrome);
+        pp.profile.TryGetSettings(out vig);
+        pp.profile.TryGetSettings(out dis);
+        pp.profile.TryGetSettings(out amo);
+    }
+
+    // Update is called once per frame
+    void Update() {
+        chrome.intensity.value = Mathf.PingPong(Time.time * 0.3f, 2);
+        vig.intensity.value = Mathf.PingPong(Time.time * 0.2f, 1.5f);
+        dis.intensity.value = Mathf.PingPong(Time.time, 60);
+        amo.intensity.value = Mathf.PingPong(Time.time, 1);
+
+        if (vig.intensity.value > 1 && !change) {
+            for (int i = 0; i < walls.Length - 1; i++) {
+                walls[i].material = wallMaterial[Random.Range(0, wallMaterial.Length)];
+            }
+            change = true;
+        }
+
+        if (vig.intensity.value < 0.5f) {
+            change = false;
+        }
+    }
+}
---
+++ Unity/ProjectedEnvironments/Assets/Scripts/Waypoints.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoints : MonoBehaviour
+{
+    public GameObject[] waypoints;
+    int current = 0;
+    float rotspeed;
+    public float speed;
+    float radius = 1f;
+
+    // Update is called once per frame
+    void Update() {
+        if (Vector3.Distance(waypoints[current].transform.position, transform.position) < radius) { //Checks if distance if less than radius
+            current = Random.Range(0, waypoints.Length);
+            if (current >= waypoints.Length) { //Checks if integer value of current is bigger or = to the last checkpoint
+                current = 0;
+            }
+        }
+        transform.position = Vector3.MoveTowards(transform.position, waypoints[current].transform.position, Time.deltaTime * speed); //Moves object towards waypoint
+    }
+}
